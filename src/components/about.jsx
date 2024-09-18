2c423021e3f1c84074c55999b73eb7d5baee796e
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-09-18 22:10:44 +0530

    added aboutvidya comp

---
+++ src/components/about.jsx
@@ -0,0 +1,21 @@
+import './about.css'
+function AboutVidya(){
+    return(
+        <div className='about-div'>
+            <h1>About Vidya</h1>
+            <p>Vidya Academy of Science & Technology (VAST) is a state-of-the-art engineering college conforming to international standards.
+                This model engineering college is approved by AICTE vide approval NO./06/ KER/ENGG/2002/91 dated 12/05/03 and affiliated to 
+                APJ Abdul Kalam Technological University. In few years VAST has evolved and achieved recognition as a notable School of Engineering
+                with its competent and committed faculty, high quality infrastructure and high technology teaching aids, and by providing a serene
+                atmosphere that complements academic life. VAST has a holistic approach to education where academic training goes hand in hand with
+                offerings that develop the body,mind and soul to prepare its graduates to be future leaders.
+            </p>
+            <img src='../img/clg-about.png' />
+
+        </div>
+        
+    );
+
+}
+
+export default AboutVidya;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import About from './about.jsx'
+import AboutVidya from './components/about.jsx';
 import Footer from './components/Footer';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App/>
+    <AboutVidya/>
     <Footer/>
   </StrictMode>,
 )
